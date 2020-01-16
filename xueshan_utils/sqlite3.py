a3918c5173e07e5b7ccd1b0db576a8202e9c9a50
xueshanlinghu <wangzhuang88888888@126.com>
2020-01-16 13:35:47 +0800

    修复sqlite3类初始化的bug

--- xueshan_utils/sqlite3.py
+++ xueshan_utils/sqlite3.py
@@ -13,7 +13,7 @@ class xsSqlite3():
     为了方便调用python中的sqlite3做交互而编写
     '''
 
-    def __init__(self, filename = ":memory"):
+    def __init__(self, filename = ":memory:"):
         '''
         初始化数据库，默认文件名为:memory:，即打开内存数据库。
         如果路径中数据库存在，则会自动创建一个新的数据库。
@@ -81,3 +81,7 @@ class xsSqlite3():
         else:
             self.c.execute(sql, param)
         return self.c.fetchall()
+
+if __name__ == '__main__':
+    # 以下是此类的测试代码
+    sqlite = xsSqlite3()
