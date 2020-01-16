1da10d52af89a7aeb53d6a778b7cc41bba7164dc
xueshanlinghu <wangzhuang88888888@126.com>
2020-01-16 10:13:47 +0800

    编写了一部分sqlite3文件内容

--- xueshan_utils/sqlite3.py
+++ xueshan_utils/sqlite3.py
@@ -7,4 +7,27 @@
 
 import sqlite3
 
+class xsSqlite3():
+    '''
+    雪山凌狐sqlite3封装工具类
+    为了方便调用python中的sqlite3做交互而编写
+    '''
 
+    def __init__(self, filename = ":memory"):
+        '''
+        初始化数据库，默认文件名为:memory:，即打开内存数据库。
+        如果路径中数据库存在，则会自动创建一个新的数据库。
+        :param filename: 文件名（包含后缀名），可以是相对路径或绝对路径
+        '''
+        self.filename = filename
+        self.conn = sqlite3.connect(self.filename)
+        self.c = self.conn.cursor()
+
+    def close_conn(self):
+        pass
+
+    def close_cursor(self):
+        pass
+
+    def close_all(self):
+        pass
