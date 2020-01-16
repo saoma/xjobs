e7c9ffb3631a1321841c6e2b4877679fe21f7bc2
xueshanlinghu <wangzhuang88888888@126.com>
2020-01-16 12:51:12 +0800

    完成sqlite3类的execute和query子程序

--- xueshan_utils/sqlite3.py
+++ xueshan_utils/sqlite3.py
@@ -46,7 +46,38 @@ class xsSqlite3():
         '''
         执行数据库的增、删、改操作
         :param sql:执行的sql语句
-        :param param: 执行sql的参数数据，比如要insert的数据，可以是list或tuple，默认为None
+        :param param: 执行sql的参数数据，比如要insert的数据，可以是list或tuple，默认为None。
+        执行单条则传入tuple即可，执行多条则传入list，元素为每条的tuple
         :return: 返回是否执行成功，成功返回True；执行后影响条数为0或执行失败都会返回False
         '''
-        pass
+        count = 0
+        try:
+            if param is None:
+                self.conn.execute(sql)
+            else:
+                if type(param) is list:
+                    self.c.cxecutemany(sql, param)
+                else:
+                    self.c.execute(sql, param)
+            count = self.conn.total_changes
+            self.conn.commit()
+        except Exception as e:
+            print("sqlite3数据库类执行sql语句报错：", e)
+            return False, e
+        if count > 0:
+            return True
+        else:
+            return False
+
+    def query(self, sql, param = None):
+        '''
+        数据库查询
+        :param sql:查询sql语句
+        :param param:sql语句参数，默认为None
+        :return:返回查询语句查询后的fetchall结果，返回一个list，如果查不到，则返回空列表
+        '''
+        if param is None:
+            self.c.execute(sql)
+        else:
+            self.c.execute(sql, param)
+        return self.c.fetchall()
