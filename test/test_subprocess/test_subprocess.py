12a15acb4978b728cb62ee853cb2399589b9ab5f
xueshanlinghu <wangzhuang88888888@126.com>
2020-01-22 15:02:50 +0800

    解决byte乱码问题：通过添加text=True命令，可使返回的结果为string而不是byte，方便后续调用和查看

--- test/test_subprocess/test_subprocess.py
+++ test/test_subprocess/test_subprocess.py
@@ -2,7 +2,7 @@
 # Author：雪山凌狐
 # website：http://www.xueshanlinghu.com
 # version： 1.0
-# update_date：2020-01-21
+# update_date：2020-01-22
 
 # 测试一下如何使用subprocess库
 
@@ -12,10 +12,9 @@ def run_python(python_path):
     cmd = "python %s" % python_path
     print("运行的命令为：" + cmd)
     try:
-        sub = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # 结论：通过添加text=True命令，可使返回的结果为string而不是byte，方便后续调用和查看
+        sub = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         out, err = sub.communicate()
-        sub.stdout.close()
-        sub.stdout.close()
         returncode = sub.returncode
         return_code = 0
         return_str = "执行成功"
@@ -23,13 +22,17 @@ def run_python(python_path):
         if returncode != 0:
             return_code = -1
             return_str = err
-
+        if out.find("操作系统") == -1:
+            return_code = -2
+            return_str = "找不到操作系统几个字"
         # 进行输出测试
         print("out:", out)
-        print(out.decode("gbk"))
+        print("err:", err)
 
     finally:
         if 'sub' in dir():
+            sub.stdout.close()
+            sub.stdout.close()
             del sub
     return return_code, return_str
 
